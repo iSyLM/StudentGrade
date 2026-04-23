666a31490183c44d3ecb1b79aadf81cecca1cf79
iSyLM <sloomy146@gmail.com>
2026-04-23 21:25:02 +0300

    add

---
+++ src/studentgrade/StudentGrade.java
@@ -0,0 +1,28 @@
+package studentgrade;
+import java.util.Scanner;
+
+public class StudentGrade {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the student mark:");
+            System.out.println("Student grade is " +
+                    getGrade(scan.nextInt()));
+
+    } 
+    //testing
+    /**
+     * Calculates grade as follows:
+     * A (90 - 100), B (80 - 89), F ( < 80 && > 100)
+     * @param mark Student mark
+     * @return The grade based on the mark entered by user
+     */
+    public static String getGrade(int mark) {
+        if (mark >= 90 && mark <= 100)
+            return "A";
+        else if (mark >= 80 && mark < 90)
+            return "B";
+        else 
+            return "F";
+    }
+}
